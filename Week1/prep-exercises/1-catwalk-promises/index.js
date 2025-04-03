05be6b77336490862edeeeb756ecee0e609f0f4f
A. Zetouny <Zet_Ahmad@hotmail.com>
2025-04-03 14:44:40 +0200

    npm install

--- Week1/prep-exercises/1-catwalk-promises/index.js
+++ Week1/prep-exercises/1-catwalk-promises/index.js
@@ -7,12 +7,12 @@ const DANCING_CAT_URL =
   'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
 
 function walk(img, startPos, stopPos) {
-  return new Promise((resolve) => {
+  return new Promise(resolve => {
     let currentPos = startPos;
 
     const repeat = setInterval(() => {
       console.log(currentPos, stopPos);
-      if(currentPos >= stopPos){
+      if (currentPos >= stopPos) {
         clearInterval(repeat);
         resolve();
       }
@@ -23,14 +23,14 @@ function walk(img, startPos, stopPos) {
 }
 
 function dance(img) {
-  return new Promise((resolve) => {
+  return new Promise(resolve => {
     const currentImgUrl = img.src;
     img.src = DANCING_CAT_URL;
 
     setTimeout(() => {
       img.src = currentImgUrl;
       resolve();
-    }, DANCE_TIME_MS)
+    }, DANCE_TIME_MS);
   });
 }
 
@@ -40,10 +40,10 @@ function catWalk() {
   const centerPos = Math.round((window.innerWidth - img.width) / 2 / 10) * 10;
   const stopPos = window.innerWidth;
 
-  document.body.style.overflow = "hidden";
-  img.style.left = startPos+"px";
+  document.body.style.overflow = 'hidden';
+  img.style.left = startPos + 'px';
 
-  const startWalking = () => 
+  const startWalking = () =>
     walk(img, startPos, centerPos)
       .then(() => dance(img))
       .then(() => walk(img, centerPos, stopPos))
