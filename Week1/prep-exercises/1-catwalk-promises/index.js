6ce77c3d9f5a6116ff08fa3f6b0471900fa4e09b
A. Zetouny <Zet_Ahmad@hotmail.com>
2025-04-03 14:42:22 +0200

    prep-exercise Solved!

--- Week1/prep-exercises/1-catwalk-promises/index.js
+++ Week1/prep-exercises/1-catwalk-promises/index.js
@@ -8,33 +8,48 @@ const DANCING_CAT_URL =
 
 function walk(img, startPos, stopPos) {
   return new Promise((resolve) => {
-    // Resolve this promise when the cat (`img`) has walked from `startPos` to
-    // `stopPos`.
-    // Make good use of the `STEP_INTERVAL_PX` and `STEP_INTERVAL_MS`
-    // constants.
+    let currentPos = startPos;
+
+    const repeat = setInterval(() => {
+      console.log(currentPos, stopPos);
+      if(currentPos >= stopPos){
+        clearInterval(repeat);
+        resolve();
+      }
+      currentPos += STEP_SIZE_PX;
+      img.style.left = `${currentPos}px`;
+    }, STEP_INTERVAL_MS);
   });
 }
 
 function dance(img) {
   return new Promise((resolve) => {
-    // Switch the `.src` of the `img` from the walking cat to the dancing cat
-    // and, after a timeout, reset the `img` back to the walking cat. Then
-    // resolve the promise.
-    // Make good use of the `DANCING_CAT_URL` and `DANCE_TIME_MS` constants.
+    const currentImgUrl = img.src;
+    img.src = DANCING_CAT_URL;
+
+    setTimeout(() => {
+      img.src = currentImgUrl;
+      resolve();
+    }, DANCE_TIME_MS)
   });
 }
 
 function catWalk() {
   const img = document.querySelector('img');
-  const startPos = -img.width;
-  const centerPos = (window.innerWidth - img.width) / 2;
+  const startPos = Math.round(-img.width / 10) * 10;
+  const centerPos = Math.round((window.innerWidth - img.width) / 2 / 10) * 10;
   const stopPos = window.innerWidth;
 
-  // Use the `walk()` and `dance()` functions to let the cat do the following:
-  // 1. Walk from `startPos` to `centerPos`.
-  // 2. Then dance for 5 secs.
-  // 3. Then walk from `centerPos` to `stopPos`.
-  // 4. Repeat the first three steps indefinitely.
+  document.body.style.overflow = "hidden";
+  img.style.left = startPos+"px";
+
+  const startWalking = () => 
+    walk(img, startPos, centerPos)
+      .then(() => dance(img))
+      .then(() => walk(img, centerPos, stopPos))
+      .then(startWalking);
+
+  startWalking();
 }
 
 window.addEventListener('load', catWalk);
